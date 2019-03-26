3db500ca60dfa2d872a18eb687e8e567c1c8001b
Aaron Bedra <aaron@aaronbedra.com>
2019-03-25 20:03:47 -0500

    Refactor HttpClient

--- include/HttpClient.h
+++ include/HttpClient.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <curl/curl.h>
+#include <functional>
+
 class HttpClient {
 private:
-    bool debug;
+  bool debug;
+  std::pair<CURLcode, std::string> executeRequest(std::string &url, std::string &token, std::function<void(CURL *curl)> callback);
 public:
-    HttpClient();
-    HttpClient(bool debug);
-    std::string get(std::string url, std::string string);
-    int post(std::string url, std::string token, std::string value);
-    int del(std::string url, std::string token);
+  HttpClient();
+  HttpClient(bool debug);
+  std::string get(std::string url, std::string string);
+  int post(std::string url, std::string token, std::string value);
+  int del(std::string url, std::string token);
 };
-
--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -1,13 +1,9 @@
-#include <curl/curl.h>
 #include <iostream>
 
 #include "HttpClient.h"
 
-static size_t writeCallback(void *contents,
-			    size_t size,
-			    size_t nmemb,
-			    void *userp)
-{
+static size_t
+writeCallback(void *contents, size_t size, size_t nmemb, void *userp) {
   ((std::string *) userp)->append((char *) contents, size * nmemb);
   return size * nmemb;
 }
@@ -18,96 +14,58 @@ HttpClient::HttpClient(bool debug) {
   this->debug = debug;
 }
 
-std::string HttpClient::get(std::string url, std::string token) {
-  // curl                                                     \
-  // -H "X-Vault-Token: f3b09679-3001-009d-2b80-9c306ab81aa6" \
-  // -X GET                                                   \
-  // http://localhost:8200/v1/secret/data/hello
+std::string
+HttpClient::get(std::string url, std::string token) {
+  auto result = executeRequest(url, token, [&](CURL *curl) {});
 
-  CURL *curl;
-  std::string readBuffer;
+  if (result.first != CURLE_OK) {
+    std::cout
+      << "GET " << url << " failed: " << curl_easy_strerror(result.first)
+      << std::endl;
 
-  curl = curl_easy_init();
-  if (curl) {
-    struct curl_slist *chunk = nullptr;
-    chunk = curl_slist_append(chunk, ("X-Vault-Token: " + token).c_str());
-
-    // TODO: SSL verify host and peer
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-
-    if (debug) {
-      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-    }
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      std::cout << "GET " << url << " failed: " << curl_easy_strerror(res)
-		<< std::endl;
-    }
-
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(chunk);
+    return "";
   }
 
-  return readBuffer;
+  return result.second;
 }
 
-int HttpClient::post(std::string url, std::string token, std::string value) {
-  // curl                                                     \
-  // -H "X-Vault-Token: f3b09679-3001-009d-2b80-9c306ab81aa6" \
-  // -H "Content-Type: application/json"                      \
-  // -X POST                                                  \
-  // -d '{"data": {"foo":"bar"}}'                             \
-  // http://localhost:8200/v1/secret/data/hello
-
-  CURL *curl;
-  CURLcode res = CURLE_SEND_ERROR;
-  std::string readBuffer;
-
-  curl = curl_easy_init();
-  if (curl) {
-    struct curl_slist *chunk = nullptr;
-    chunk = curl_slist_append(chunk, ("X-Vault-Token: " + token).c_str());
-    chunk = curl_slist_append(chunk, "Content-Type: application/json");
-
-    // TODO: SSL verify host and peer
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+int
+HttpClient::post(std::string url, std::string token, std::string value) {
+  auto result = executeRequest(url, token, [&](CURL *curl) {
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, value.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+  });
 
-    if (debug) {
-      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-    }
-
-    res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK) {
-      std::cout << "POST " << url << " failed: " << curl_easy_strerror(res)
-		<< std::endl;
-    }
-
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(chunk);
+  if (result.first != CURLE_OK) {
+    std::cout
+      << "POST " << url << " failed: " << curl_easy_strerror(result.first)
+      << std::endl;
   }
 
-  return res;
+  return result.first;
 }
 
-int HttpClient::del(std::string url, std::string token) {
-  // curl                                                     \
-  // -H "X-Vault-Token: f3b09679-3001-009d-2b80-9c306ab81aa6" \
-  // -H "Content-Type: application/json"                      \
-  // -X DELETE                                                \
-  // http://localhost:8200/v1/secret/data/hello
+int
+HttpClient::del(std::string url, std::string token) {
+  auto result = executeRequest(url, token, [&](CURL *curl) {
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+  });
 
+  if (result.first != CURLE_OK) {
+    std::cout
+      << "DELETE " << url << " failed: " << curl_easy_strerror(result.first)
+      << std::endl;
+  }
+
+  return result.first;
+}
+
+std::pair<CURLcode, std::string>
+HttpClient::executeRequest(std::string &url,
+                           std::string &token,
+                           std::function<void(CURL *curl)> callback) {
   CURL *curl;
   CURLcode res = CURLE_SEND_ERROR;
-  std::string readBuffer;
+  std::string buffer;
 
   curl = curl_easy_init();
   if (curl) {
@@ -118,24 +76,19 @@ int HttpClient::del(std::string url, std::string token) {
     // TODO: SSL verify host and peer
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
 
     if (debug) {
       curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
     }
 
+    callback(curl);
+
     res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK) {
-      std::cout << "DELETE " << url << " failed: " << curl_easy_strerror(res)
-		<< std::endl;
-    }
-
     curl_easy_cleanup(curl);
     curl_slist_free_all(chunk);
   }
 
-  return res;
+  return std::make_pair(res, buffer);
 }
