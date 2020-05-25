b160f745a148a56c2a9c108248a0879531e1900d
Aaron Bedra <aaron@aaronbedra.com>
2020-05-25 02:49:38 -0500

    Tests

--- example/postgresql/select.py
+++ example/postgresql/select.py
@@ -1,7 +1,24 @@
+import hvac
+import os
 import psycopg2
+
+from dotenv import load_dotenv
+from pathlib import Path
 from pprint import pprint
 
-conn = psycopg2.connect(host="localhost", database="vault", user="postgres", password="postgres")
+load_dotenv(dotenv_path=Path('..') / '..' / '.env')
+
+role_id = os.getenv("APPROLE_ROLE_ID")
+secret_id = os.getenv("APPROLE_SECRET_ID")
+
+vault_client = hvac.Client(url="http://localhost:8200")
+vault_client.auth_approle(role_id=role_id, secret_id=secret_id)
+
+postgres_credentials = vault_client.read(path="database/creds/readonly")
+postgres_username = postgres_credentials["data"]["username"]
+postgres_password = postgres_credentials["data"]["password"]
+
+conn = psycopg2.connect(host="localhost", database="vault", user=postgres_username, password=postgres_password)
 cursor = conn.cursor()
 cursor.execute("select * from things")
 
