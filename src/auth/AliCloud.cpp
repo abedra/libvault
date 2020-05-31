2706b9865ae317508257e7ee89745e5e09ac1175
Aaron Bedra <aaron@aaronbedra.com>
2020-05-31 18:08:09 -0500

    Implements AliCloud auth and secrets

---
+++ src/auth/AliCloud.cpp
@@ -0,0 +1,25 @@
+#include "VaultClient.h"
+
+std::optional<std::string> Vault::AliCloud::Auth::listRoles() {
+  return HttpConsumer::list(client_, getUrl(Path{"roles"}));
+}
+
+std::optional<std::string> Vault::AliCloud::Auth::readRole(const Path &path) {
+  return HttpConsumer::get(client_, getUrl(Path{"role/" + path}));
+}
+
+std::optional<std::string> Vault::AliCloud::Auth::createRole(const Path &path, const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"role/" + path}), parameters);
+}
+
+std::optional<std::string> Vault::AliCloud::Auth::deleteRole(const Path &path) {
+  return HttpConsumer::del(client_, getUrl(Path{"role/" + path}));
+}
+
+std::optional<std::string> Vault::AliCloud::Auth::login(const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"login"}), parameters);
+}
+
+Vault::Url Vault::AliCloud::Auth::getUrl(const Vault::Path& path) {
+  return client_.getUrl("/v1/auth/alicloud", path);
+}
---
+++ src/engines/AliCloud.cpp
@@ -0,0 +1,33 @@
+#include "VaultClient.h"
+
+std::optional<std::string> Vault::AliCloud::Secrets::readConfig() {
+  return HttpConsumer::get(client_, getUrl(Path{"config"}));
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::createConfig(const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"config"}), parameters);
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::listRoles() {
+  return HttpConsumer::list(client_, getUrl(Path{"roles"}));
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::readRole(const Path &path) {
+  return HttpConsumer::get(client_, getUrl(Path{"role/" + path}));
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::createRole(const Path &path, const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"role/" + path}), parameters);
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::deleteRole(const Path &path) {
+  return HttpConsumer::del(client_, getUrl(Path{"role/" + path}));
+}
+
+std::optional<std::string> Vault::AliCloud::Secrets::generateCredentials(const Path &path) {
+  return HttpConsumer::get(client_, getUrl(Path{"creds/" + path}));
+}
+
+Vault::Url Vault::AliCloud::Secrets::getUrl(const Vault::Path& path) {
+  return client_.getUrl("/v1/alicloud", path);
+}
