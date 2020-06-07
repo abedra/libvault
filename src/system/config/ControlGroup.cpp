c25ba9d144ec934ec4379ffc62ce6e03430649de
Aaron Bedra <aaron@aaronbedra.com>
2020-06-07 13:27:15 -0500

    Add support for sys/control-group (#45)

--- src/system/config/ControlGroup.cpp
+++ src/system/config/ControlGroup.cpp
@@ -1,17 +1,25 @@
 #include "VaultClient.h"
 
-std::optional<std::string> Vault::Sys::ControlGroup::read(const Path &path) {
-  return HttpConsumer::get(client_, getUrl());
+std::optional<std::string> Vault::Sys::ControlGroup::read() {
+  return HttpConsumer::get(client_, getUrl(Path{"config/control-group"}));
 }
 
-std::optional<std::string> Vault::Sys::ControlGroup::configure(const Path &path, const Parameters &parameters) {
-  return HttpConsumer::put(client_, getUrl(), parameters);
+std::optional<std::string> Vault::Sys::ControlGroup::configure(const Parameters &parameters) {
+  return HttpConsumer::put(client_, getUrl(Path{"config/control-group"}), parameters);
 }
 
-std::optional<std::string> Vault::Sys::ControlGroup::del(const Path &path) {
-  return HttpConsumer::del(client_, getUrl());
+std::optional<std::string> Vault::Sys::ControlGroup::del() {
+  return HttpConsumer::del(client_, getUrl(Path{"config/control-group"}));
 }
 
-Vault::Url Vault::Sys::ControlGroup::getUrl() {
-  return client_.getUrl("/v1/sys/config/control-group", Path{});
+std::optional<std::string> Vault::Sys::ControlGroup::authorize(const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"control-group/authorize"}), parameters);
+}
+
+std::optional<std::string> Vault::Sys::ControlGroup::request(const Parameters &parameters) {
+  return HttpConsumer::post(client_, getUrl(Path{"control-group/request"}), parameters);
+}
+
+Vault::Url Vault::Sys::ControlGroup::getUrl(const Path &path) {
+  return client_.getUrl("/v1/sys/", path);
 }
