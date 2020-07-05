30aa04fdfdb3442c6598814781917f4002a9a2ee
Aaron Bedra <aaron@aaronbedra.com>
2020-07-05 15:48:49 -0500

    Implements sys/namespaces

--- include/VaultClient.h
+++ include/VaultClient.h
@@ -657,6 +657,21 @@ namespace Vault {
       };
     };
 
+    class Namespaces {
+    public:
+      explicit Namespaces(const Client &client) : client_(client) {}
+
+      std::optional<std::string> list();
+      std::optional<std::string> read(const Path &path);
+      std::optional<std::string> create(const Path &path, const Parameters &parameters);
+      std::optional<std::string> del(const Path &path);
+
+    private:
+      Url getUrl(const Path &path);
+
+      const Client &client_;
+    };
+
   private:
     Url getUrl(const Path &path);
 
