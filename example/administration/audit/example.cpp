9ea84bf43c4faccf1a2a1e98aca4851705ae5b26
Aaron Bedra <aaron@aaronbedra.com>
2020-12-23 08:17:04 -0600

    Audit read example

---
+++ example/administration/audit/example.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "VaultClient.h"
+#include "../../shared/shared.h"
+
+int main(void) {
+  char *rootTokenEnv = std::getenv("VAULT_ROOT_TOKEN");
+  if (!rootTokenEnv) {
+    std::cout << "The VAULT_ROOT_TOKEN environment variable must be set" << std::endl;
+    exit(-1);
+  }
+  Vault::Token rootToken{rootTokenEnv};
+  Vault::Client rootClient = getRootClient(rootToken);
+
+  if (rootClient.is_authenticated()) {
+    Vault::Sys::Audit auditAdmin{rootClient};
+    auto response = auditAdmin.read();
+    if (response) {
+      std::cout << response.value() << std::endl;
+    } else {
+      std::cout << "Failure" << std::endl;
+    }
+  } else {
+    std::cout << "Unable to authenticate" << std::endl;
+  }
+}
