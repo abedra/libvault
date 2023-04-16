cca5769bffbf40c15c578bbb9fc7cc3727babd0e
Aaron Bedra <aaron@aaronbedra.com>
2023-04-16 17:47:38 -0500

    Fix Integration Tests

--- test/integration/engines/DatabaseTests.cpp
+++ test/integration/engines/DatabaseTests.cpp
@@ -1,40 +1,40 @@
 #include <catch2/catch.hpp>
 
-#include "VaultClient.h"
 #include "../TestHelpers.h"
+#include "VaultClient.h"
 
 TEST_CASE("PostgreSQL") {
-    Vault::Client vaultClient(TestHelpers::AppRole::login());
-    Vault::Database postgres(vaultClient);
-    Vault::Path path{"postgresql"};
-    Vault::Parameters configuration({
-        {"plugin_name", "postgresql-database-plugin"},
-        {"allowed_roles", "readonly"},
-        {"connection_url", "postgresql://postgres:postgres@localhost:5432/vault?sslmode=disable"}
-    });
+  Vault::Client vaultClient(TestHelpers::AppRole::login());
+  Vault::Database postgres(vaultClient);
+  Vault::Path path{"postgresql"};
+  Vault::Parameters configuration(
+      {{"plugin_name", "postgresql-database-plugin"},
+       {"allowed_roles", "readonly"},
+       {"connection_url",
+        "postgresql://postgres:postgres@docker-postgres-1:5432/"
+        "vault?sslmode=disable"}});
 
-    Vault::Parameters role({
-        {"db_name", "postgresql"},
-        {"default_ttl", "1h"},
-        {"max_ttl", "24h"},
-        {"creation_statements", R"(CREATE ROLE "{{name}}" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO "{{name}}";)"}
-    });
+  Vault::Parameters role(
+      {{"db_name", "postgresql"},
+       {"default_ttl", "1h"},
+       {"max_ttl", "24h"},
+       {"creation_statements",
+        R"(CREATE ROLE "{{name}}" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO "{{name}}";)"}});
 
-    Vault::Path rolePath{"readonly"};
-    postgres.configureConnection(path, configuration);
-    postgres.deleteRole(rolePath);
-    postgres.createRole(rolePath, role);
+  Vault::Path rolePath{"readonly"};
+  postgres.configureConnection(path, configuration);
+  postgres.deleteRole(rolePath);
+  postgres.createRole(rolePath, role);
 
-    SECTION("Generate Credentials")
-    {
-        auto response = postgres.generateCredentials(rolePath);
+  SECTION("Generate Credentials") {
+    auto response = postgres.generateCredentials(rolePath);
 
-        if (response) {
-            auto data = nlohmann::json::parse(response.value())["data"];
-            CHECK(!data["username"].empty());
-            CHECK(!data["password"].empty());
-        } else {
-            CHECK(false);
-        }
+    if (response) {
+      auto data = nlohmann::json::parse(response.value())["data"];
+      REQUIRE(!data["username"].empty());
+      REQUIRE(!data["password"].empty());
+    } else {
+      REQUIRE(false);
     }
+  }
 }
--- test/integration/engines/RabbitMqTests.cpp
+++ test/integration/engines/RabbitMqTests.cpp
@@ -1,38 +1,35 @@
 #include <catch2/catch.hpp>
 
-#include "VaultClient.h"
 #include "../TestHelpers.h"
+#include "VaultClient.h"
 
 TEST_CASE("RabbitMQ") {
   Vault::Client vaultClient = TestHelpers::AppRole::login();
   Vault::RabbitMq rabbitMq(vaultClient);
-  Vault::Parameters configuration({
-    {"connection_uri", "http://localhost:15672"},
-    {"username", "guest"},
-    {"password", "guest"}
-  });
+  Vault::Parameters configuration(
+      {{"connection_uri", "http://docker-rabbitmq-1:15672"},
+       {"username", "guest"},
+       {"password", "guest"}});
 
-  Vault::Parameters role({
-    {"vhosts", R"({"/":{"write": ".*", "read": ".*", "configure": ".*"}})"},
-    {"tags", "administrator"}
-  });
+  Vault::Parameters role(
+      {{"vhosts", R"({"/":{"write": ".*", "read": ".*", "configure": ".*"}})"},
+       {"tags", "administrator"}});
 
   const Vault::Path &path = Vault::Path{"my-role"};
 
   rabbitMq.configureConnection(configuration);
   rabbitMq.deleteRole(path);
   rabbitMq.createRole(path, role);
-  
-  SECTION("Generate Configuration")
-  {
+
+  SECTION("Generate Configuration") {
     auto response = rabbitMq.generateCredentials(path);
 
     if (response) {
       auto data = nlohmann::json::parse(response.value())["data"];
-      CHECK(!data["username"].empty());
-      CHECK(!data["password"].empty());
+      REQUIRE(!data["username"].empty());
+      REQUIRE(!data["password"].empty());
     } else {
-      CHECK(false);
+      REQUIRE(false);
     }
   }
 }
